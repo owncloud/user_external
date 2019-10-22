21a5e1e2667d4a68145cd83ebe89145164c6a5d8
lucamemini <luca@memini.it>
2019-10-22 12:09:37 +0200

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,5 +1,5 @@
 <?php
-## little change for issue #9 https://github.com/owncloud/user_external/issues/9
+// little change for issue #9 https://github.com/owncloud/user_external/issues/9
 include_once('../lib/imap.php');
 include_once('../lib/smb.php');
 include_once('../lib/ftp.php');
