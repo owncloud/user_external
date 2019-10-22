6ec7029c06eb2508e35e92ad16882d92f8031ca2
lucamemini <luca@memini.it>
2019-10-22 11:58:33 +0200

    Update app.php

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,4 +1,11 @@
 <?php
+## ugly patch for issue #9 https://github.com/owncloud/user_external/issues/9
+include_once('../lib/imap.php');
+include_once('../lib/smb.php');
+include_once('../lib/ftp.php');
+
+/*
 OC::$CLASSPATH['OC_User_IMAP']='user_external/lib/imap.php';
 OC::$CLASSPATH['OC_User_SMB']='user_external/lib/smb.php';
 OC::$CLASSPATH['OC_User_FTP']='user_external/lib/ftp.php';
+*/
